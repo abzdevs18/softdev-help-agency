634b9a78252e93207d08b035950bc07608611f7e
Clint.Abz <abz.devs@gmail.com>
2019-08-18 00:23:47 +0800

    Changing the feedback for the company profile details

--- company-profile.php
+++ company-profile.php
@@ -62,37 +62,46 @@
 						<h2>Client history and feedback</h2>
 					</div>
 					<div id="works">
-						<div class="list-of-feedback">
-							<h2>VBA Project</h2>
-							<div class="rate-date">
-								<div id="emp-rate">
-									<span>
-										0.0
-									</span>
-									<div id="s5" style="display: flex;flex-direction: row;">
-										<div class="rating-star">
-											<i class="fas fa-star"></i>
-											<i class="fas fa-star"></i>
-											<i class="fas fa-star"></i>
-											<i class="fas fa-star"></i>
-											<i class="fas fa-star-half-alt"></i>						
+						<div class="feed-item">
+							<div class="feed-wrapper">
+								<div class="employer-image">
+									<img src="<?php echo root_folder . '/admin/assets/img/icons/img-06.jpg';?>">
+								</div>
+								<div id="feed-details">
+									<div class="list-of-feedback">
+										<h2>VBA Project</h2>
+										<div class="rate-date">
+											<div id="emp-rate">
+												<span>
+													0.0
+												</span>
+												<div id="s5" style="display: flex;flex-direction: row;">
+													<div class="rating-star">
+														<i class="fas fa-star"></i>
+														<i class="fas fa-star"></i>
+														<i class="fas fa-star"></i>
+														<i class="fas fa-star"></i>
+														<i class="fas fa-star-half-alt"></i>						
+													</div>
+													<span>( 0 reviews )</span>
+												</div>
+											</div>
+											<div>
+												<p>Nov 2019</p>
+											</div>
+										</div><!-- End rating -->
+										<div class="feedback-description">
+											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+											tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+											quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+											consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+											cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+											proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 										</div>
-										<span>( 0 reviews )</span>
-									</div>
-								</div>
-								<div>
-									<p>Nov 2019</p>
-								</div>
-							</div><!-- End rating -->
-							<div class="feedback-description">
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-								tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-								quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-								consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-								cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-								proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+									</div><!-- End of List-of-Feedback -->
+								</div>					
 							</div>
-						</div><!-- End of List-of-Feedback -->
+						</div>
 					</div>
 				</div><!-- End of Work history and Feedback --> 
 			</div>
